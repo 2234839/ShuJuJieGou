d155bedc6fcb48ecd77f51b89570403298cc274f
崮生 <2234839456@qq.com>
2017-09-15 20:53:39 +0800

    Update 约瑟夫链表_递归杀人术.cpp
    终于把递归杀人写出来了

--- 约瑟夫链表_递归杀人术.cpp
+++ 约瑟夫链表_递归杀人术.cpp
@@ -1,13 +1,15 @@
 #include "stdafx.h"
 #include <iostream>
-	using namespace std;
+using namespace std;
 typedef struct  Node
 {
 	int data;
 	struct Node *next;
 }LiList;
+int h = 30;//设置共有多少人
+int ii = 1;//用来计数
 
-void CreateListR(LiList *&L, int a[], int n) {
+void CreateListR(LiList *&L, int a[], int n) {//尾插法创建单链表
 	LiList *s, *r;
 	int i; L = new LiList;
 	r = L;
@@ -20,83 +22,56 @@ void CreateListR(LiList *&L, int a[], int n) {
 	}
 	r->next = NULL;
 }
-int h = 30;
-void shuchu(LiList *&L) {
-	Node *p = L;
-	p = p->next;
-	for (int i = 0; i <h; i++)
-	{
-		//cout << p << ":" << p->data << "\t";
-		p = p->next;
-	}
-	//cout << "\n";
-}
-
-int ii = 1;
-Node *t;
-void 后移(LiList *&p, LiList *&L) {
-	if (p->next=NULL)
-		p = L->next;
-	else
-		p = p->next;
-}
-int diguisharen(LiList *&L, Node *p, Node *s) {
-	
-	//cout << p<<":"<<p->data<<"\n";
-	if (ii == 8) {
+int diguisharen(LiList *&L, Node *p,int x) {
+	if (h == x)//递归终止条件
+		return x;
+	if (ii == 8) {//该杀人了
 		ii = 1; //重新开始数数
 		Node *r;
 		r = p->next;//r成为了第九个节点
-		if (r->next != NULL)
-			p->next = r->next;//连接第九与第十个节点
+
+		if (r->next != NULL)//此判断结构用于链接表尾与表头  防止非法访问地址
+			p->next = r->next;//连接p节点前一个与后面一个个节点
 		else
 			p->next = L->next;
-		cout << "击杀" << r->data << "号\n";
-		delete r;//删除第九个节点
+
+		cout << "杀" << r->data << "号\t";
+		delete r;//删除节点
+
 		if (p->next == NULL)
 			p = L->next;
 		else
 			p = p->next;
 
 		h--;
-		return diguisharen(L, p, s);
+		return diguisharen(L, p,x);
 	}
-	else
+	else//往下数人头
 	{
-		if (p->next == NULL) {
-			p = t->next;
+		if (p->next == NULL) {//从表尾跳到表头
+			p = L->next;
 			ii++;
-			//cout << "L的请况" << p->data << "||" <<p->next << "\n";
-			shuchu(L);
-			return diguisharen(L, p, s);
+			return diguisharen(L, p,x);
 		}
-		else
+		else//读取下一个节点
 		{
 			p = p->next;
 			ii++;
-			return diguisharen(L, p, s);
+			return diguisharen(L, p,x);
 		}
 	}
-	return diguisharen(L, p, s);
-
-
 }
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int a[30];
-	for (int i = 1; i < h + 1; i++)
-	{
+	for (int i = 1; i < h + 1; i++)//数组赋值
 		a[i - 1] = i;
-	}
 	LiList *L;
 	CreateListR(L, a, h);
-	t=L;
-	shuchu(L);
 	Node *p = L->next, *s = L->next;
-	cout << "返回值" << diguisharen(L, p, s);
-	cout << "返回值" << diguisharen(L, p, s);
-	shuchu(L);
+	cout<<"\n还存活"<<diguisharen(L, p,1);//L=人员链表  p=用于处理的节点   杀到剩1人
+	cout << "人\n";
 	return 0;
 }
